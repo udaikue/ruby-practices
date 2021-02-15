9298ae5efd551ce59f2d9adf4f823eff2c110f27
udaikue <ikueikue@gmail.com>
2021-02-15 14:15:12 +0900

    繰り返し処理をまとめる

--- 07.bowling_object/lib/frame.rb
+++ 07.bowling_object/lib/frame.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 class Frame
-  def initialize(shot1_mark, shot2_mark, shot3_mark = nil)
-    @shot1 = Shot.new(shot1_mark)
-    @shot2 = Shot.new(shot2_mark)
-    @shot3 = Shot.new(shot3_mark)
+  def initialize(shots)
+    @shot_sum = 0
+    shots.map do |shot|
+      @shot = Shot.new(shot)
+      @shot_sum += @shot.score
+    end
   end
 
   def score
-    @shot1.score + @shot2.score + @shot3.score
+    @shot_sum
   end
 end
