f8ac7e00c14a92eb0fcfaf3bd21f0761540b209e
udaikue <ikueikue@gmail.com>
2021-02-15 14:07:17 +0900

    テストメソッドの修正

--- 07.bowling_object/test/bowling_test.rb
+++ 07.bowling_object/test/bowling_test.rb
@@ -1,19 +1,34 @@
 # frozen_string_literal: true
 
 require 'minitest/autorun'
-require './lib/bowling'
+require_relative '../lib/game'
+require_relative '../lib/frame'
+require_relative '../lib/bonus'
+require_relative '../lib/shot'
 
 class BowlingTest < Minitest::Test
-  def test_bowling
-    game = Game.new('23558190X4263153730')
-    assert_equal 94, game.score
+  def test_perfect
     game = Game.new('XXXXXXXXXXXX')
     assert_equal 300, game.score
-    game = Game.new('6390038273X9180X645')
-    assert_equal 139, game.score
+  end
+
+  def test_frame10_all_strike
     game = Game.new('6390038273X9180XXXX')
     assert_equal 164, game.score
+  end
+
+  def test_frame10_strike
     game = Game.new('552082376490XX32X36')
     assert_equal 133, game.score
+  end
+
+  def test_frame10_spare
+    game = Game.new('6390038273X9180X645')
+    assert_equal 139, game.score
+  end
+
+  def test_not_special
+    game = Game.new('23558190X4263153730')
+    assert_equal 94, game.score
   end
 end
